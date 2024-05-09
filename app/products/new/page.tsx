ad85e37949bf071d2147144ee91c3440a6ab5c18
julord87 <julianmzg@gmail.com>
2024-05-09 10:51:33 -0300

    first commit

---
+++ app/products/new/page.tsx
@@ -0,0 +1,6 @@
+
+export default function CreateProductPage() {
+  return (
+    <div>CreateProductPage</div>
+  )
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,6 @@
+
+export default function ProductsPage() {
+  return (
+    <div>ProductsPage</div>
+  )
+}
