088506df06c33f59ff0b8be19e822883521a18d3
julord87 <julianmzg@gmail.com>
2024-05-11 08:43:26 -0300

    products UI done

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,6 +1,31 @@
+import ProductCard from "@/components/products/ProductCard"
+import { prisma } from "@/src/lib/prisma"
 
-export default function OrderPage() {
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category
+      }
+    }
+  })
+  return products
+}
+
+export default async function OrderPage({params} : {params: {category : string}}) {
+  const products = await getProducts(params.category)
+  console.log(products)
   return (
-    <div>OrderPage</div>
+    <>
+      <h1 className="text-2xl my-10">
+        Elige y personaliza tu pedido a continuación
+      </h1>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
   )
 }
--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,28 +1,27 @@
-import Image from "next/image"
-import Link from "next/link"
-import { Category } from "@prisma/client"
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { Category } from "@prisma/client";
 
 type CategoryIconProps = {
-    category: Category
-}
+  category: Category;
+};
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
+  const params = useParams<{category: string}>();
+
   return (
     <div
-        className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
+      className={`${category.slug === params.category ? "bg-amber-200" : ""} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
     >
-        <div className="w-16 h-16 relative">
-            <Image 
-                fill
-                src={`/icon_${category.slug}.svg`} 
-                alt={category.name}
-            />
-        </div>
+      <div className="w-16 h-16 relative">
+        <Image fill src={`/icon_${category.slug}.svg`} alt={category.name} />
+      </div>
 
-        <Link 
-            className="text-xl font-bold"
-            href={`/order/${category.slug}`}
-        >{category.name}</Link>
+      <Link className="text-xl font-bold" href={`/order/${category.slug}`}>
+        {category.name}
+      </Link>
     </div>
-  )
+  );
 }
