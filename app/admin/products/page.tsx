5092a3bbed97fcff96df6678fca2ab2e14187de1
julord87 <julianmzg@gmail.com>
2024-05-18 09:37:50 -0300

    admin products pagination

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,37 @@
+import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function ProductsPage() {
+async function getProducts(page : number, pageSize : number) {
+  const skip = (page - 1) * pageSize
+
+  const products = await prisma.product.findMany({
+    take: pageSize,
+    skip,
+    include: {
+      category: true
+    }
+  })
+  return products
+}
+
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
+
+export default async function ProductsPage({searchParams} : {searchParams: {page: string}}) {
+
+  const page = +searchParams.page || 1
+  const pageSize = 10
+
+
+  const products = await getProducts(page, pageSize)
+
   return (
     <>
       <Heading>Administrar Productos</Heading>
+
+      <ProductTable 
+        products={products}
+      />
     </>
   )
 }
