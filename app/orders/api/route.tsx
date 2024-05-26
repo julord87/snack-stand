76cb677a953125cf4f6142f73d85060497281f8d
julord87 <julianmzg@gmail.com>
2024-05-26 11:55:53 -0300

    routes fix

--- app/orders/api/route.tsx
+++ app/orders/api/route.tsx
@@ -1,5 +1,7 @@
 import { prisma } from "@/src/lib/prisma";
 
+export const dynamic = "force-dynamic"
+
 export async function GET() {
     const orders = await prisma.order.findMany({
         take: 5,
