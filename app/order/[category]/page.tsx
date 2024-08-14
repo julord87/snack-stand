e930a81323095ee260d57af2e3401c29bda3cc32
julord87 <julianmzg@gmail.com>
2024-08-14 12:12:15 -0300

    chore: Add lazy loading for images on order page

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,6 +1,7 @@
 import ProductCard from "@/components/products/ProductCard"
 import Heading from "@/components/ui/Heading"
 import { prisma } from "@/src/lib/prisma"
+import { Product } from "@prisma/client"
 
 async function getProducts(category: string) {
   const products = await prisma.product.findMany({
@@ -22,7 +23,7 @@ export default async function OrderPage({params} : {params: {category : string}}
       </Heading>
 
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 items-start">
-        {products.map((product) => (
+        {products.map((product : Product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
