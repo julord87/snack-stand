c976fcb81bc94333d4b5a90d052f5c3b97eb698a
Julian Martinez Gomez <julianmzg@gmail.com>
2024-08-15 08:31:15 -0300

    chore: Refactor order page, fix formatting, add type annotation, and improve performance

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ProductCard from "@/components/products/ProductCard"
 import Heading from "@/components/ui/Heading"
 import { prisma } from "@/src/lib/prisma"
@@ -30,37 +29,3 @@ export default async function OrderPage({params} : {params: {category : string}}
     </>
   )
 }
-=======
-import ProductCard from "@/components/products/ProductCard"
-import Heading from "@/components/ui/Heading"
-import { prisma } from "@/src/lib/prisma"
-import { Product } from "@prisma/client"
-
-async function getProducts(category: string):Promise<Product[]> {
-  const products = await prisma.product.findMany({
-    where: {
-      category: {
-        slug: category
-      }
-    }
-  })
-  return products
-}
-
-export default async function OrderPage({params} : {params: {category : string}}) {
-  const products : Product[] = await getProducts(params.category)
-  return (
-    <>
-      <Heading>
-        Elige y personaliza tu pedido a continuación
-      </Heading>
-
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 items-start">
-        {products.map((product : Product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
-    </>
-  )
-}
->>>>>>> f77773c0dcc8815c9489cc577c696807f9b662fd
